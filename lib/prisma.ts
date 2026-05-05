88c1ebe9fec40f70e0b294b1743a5ff9f9550035
Isaac Mushimiyimana <mushimiyimanais123@gmail.com>
2026-05-05 17:43:06 +0200

    Solve problem of prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,23 +1,23 @@
-import { PrismaClient } from "@/lib/generated/prisma/client";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "@/lib/generated/prisma/client";
+// import { Pool } from "pg";
+// import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = globalThis as unknown as {
-    prisma: PrismaClient | undefined;
-};
+// const globalForPrisma = globalThis as unknown as {
+//     prisma: PrismaClient | undefined;
+// };
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+// });
 
-const adapter = new PrismaPg(pool);
+// const adapter = new PrismaPg(pool);
 
-export const prisma =
-    globalForPrisma.prisma ??
-    new PrismaClient({
-        adapter,
-    });
+// export const prisma =
+//     globalForPrisma.prisma ??
+//     new PrismaClient({
+//         adapter,
+//     });
 
-if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = prisma;
-}
+// if (process.env.NODE_ENV !== "production") {
+//     globalForPrisma.prisma = prisma;
+// }
