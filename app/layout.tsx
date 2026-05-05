284c6be2526cdf641fb7c8e1fcac273fcc14df13
Isaac Mushimiyimana <mushimiyimanais123@gmail.com>
2026-05-05 06:49:09 +0200

    First commit on 05/05/2026:

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html
       lang="en"
+      data-scroll-behavior="smooth"
       suppressHydrationWarning
       className={`${dmSans.variable} ${jetbrainsMono.variable}`}
     >
